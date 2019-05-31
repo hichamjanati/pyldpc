b69d7420c5e2a5148e744ec49cd8a32195d8abd3
Hicham Janati <hicham.janati@inria.fr>
2019-06-01 01:10:57 +0200

    fix setup and add makefile

---
+++ Makefile
@@ -0,0 +1,64 @@
+# simple makefile to simplify repetetive build env management tasks under posix
+
+PYTHON ?= python
+CYTHON ?= cython
+PYTESTS ?= pytest
+
+CTAGS ?= ctags
+
+all: clean inplace test
+
+clean-pyc:
+	find . -name "*.pyc" | xargs rm -f
+	find . -name "__pycache__" | xargs rm -rf
+
+clean-so:
+	find . -name "*.so" | xargs rm -f
+	find . -name "*.pyd" | xargs rm -f
+	find . -name "*.cpp" | xargs rm -f
+
+clean-build:
+	rm -rf build
+
+clean-ctags:
+	rm -f tags
+
+clean: clean-build clean-pyc clean-so clean-ctags
+
+in: inplace # just a shortcut
+inplace:
+	$(PYTHON) setup.py build_ext -i
+
+test-code:
+	$(PYTESTS) pyldpc
+
+test-doc:
+	$(PYTESTS) $(shell find doc -name '*.rst' | sort)
+
+test-coverage:
+	rm -rf coverage .coverage
+	$(PYTESTS) pyldpc --cov=pyldpc --cov-report html:coverage
+
+test: test-code test-doc test-manifest
+
+trailing-spaces:
+	find . -name "*.py" | xargs perl -pi -e 's/[ \t]*$$//'
+
+cython:
+	find -name "*.pyx" | xargs $(CYTHON)
+
+ctags:
+	# make tags for symbol based navigation in emacs and vim
+	# Install with: sudo apt-get install exuberant-ctags
+	$(CTAGS) -R *
+
+.PHONY : doc-plot
+doc-plot:
+	make -C doc html
+
+.PHONY : doc
+doc:
+	make -C doc html-noplot
+
+test-manifest:
+	check-manifest --ignore doc,pyldpc/*/tests,.circleci*,*.c;
--- setup.py
+++ setup.py
@@ -1,34 +1,41 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
+import numpy
 
 
 def readme():
-	with open('README.rst') as f:
-		return f.read()
+    with open('README.rst') as f:
+        return f.read()
+
+
+INSTALL_REQUIRES = ['numpy', 'scipy']
+
+EXTRAS_REQUIRE = {'tests': ['pytest', 'pytest-cov'],
+                  'docs': ['sphinx', 'sphinx-gallery',
+                           'sphinx_rtd_theme', 'numpydoc',
+                           'matplotlib', 'download']
+                  }
 
 if __name__ == "__main__":
-
-	setup(name='pyldpc',
-		version='0.7.5',
-		description='Simulation of Low Density Parity Check Codes ldpc',
-		long_description=readme(),
-		classifiers=[
-			'Programming Language :: Python :: 2.7',
-			'Programming Language :: Python :: 3.5',
-			'Development Status :: 4 - Beta',
-			'Environment :: MacOS X',
-			'Framework :: IPython',
-			'Intended Audience :: Developers',
-			'Intended Audience :: Education',
-			'Intended Audience :: Science/Research',
-			'Intended Audience :: Telecommunications Industry',
-			'Natural Language :: English',
-			'Natural Language :: French'
-		],
-		keywords='codes ldpc error detection decoding coding pyldpc',
-		url='https://github.com/hichamjanati/pyldpc',
-		author='Hicham Janati',
-		author_email='hicham.janati@ensae.fr',
-		license='MIT',
-		packages = ['pyldpc'],
-		install_requires = ['numpy','scipy'],
-		zip_safe=False)
+    setup(packages=find_packages(),
+          include_dirs=[numpy.get_include()],
+          install_requires=INSTALL_REQUIRES,
+          extras_require=EXTRAS_REQUIRE,
+          version='0.7.5',
+          description='Simulation of Low Density Parity Check Codes ldpc',
+          long_description=readme(),
+          classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Telecommunications Industry',
+          'Natural Language :: English',
+          ],
+          keywords='codes ldpc error detection decoding coding pyldpc',
+          url='https://github.com/hichamjanati/pyldpc',
+          author='Hicham Janati',
+          author_email='hicham.janati@inria.fr',
+          license='MIT',
+          )
