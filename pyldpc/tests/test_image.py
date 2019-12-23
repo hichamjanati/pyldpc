8f3ac8d05dad4dd0aa4e468fc3948ddf5b23fa47
Hicham Janati <hicham.janati@inria.fr>
2019-12-23 13:39:04 -0800

    fix tests in image decoding

--- pyldpc/tests/test_image.py
+++ pyldpc/tests/test_image.py
@@ -10,9 +10,9 @@ from itertools import product
                          product([False, True], [True], [False, True]))
 def test_image_gray(systematic, log, sparse):
 
-    n = 10
-    d_v = 1
-    d_c = 2
+    n = 21
+    d_v = 2
+    d_c = 3
     seed = 0
     rnd = np.random.RandomState(seed)
     H, G = make_ldpc(n, d_v, d_c, seed=seed, systematic=systematic,
@@ -24,24 +24,21 @@ def test_image_gray(systematic, log, sparse):
 
     img = rnd.randint(0, 255, size=(3, 3))
     img_bin = gray2bin(img)
+
     coded, noisy = ldpc_images.encode_img(G, img_bin, snr, seed)
 
     x = ldpc_images.decode_img(G, H, coded, snr, maxiter=100, log=log)
 
     assert ldpc_images.ber_img(img_bin, gray2bin(x)) == 0
 
-# sparse = True
-# log = True
-# systematic = True
-
 
 @pytest.mark.parametrize("systematic, log, sparse",
                          product([False, True], [True], [False, True]))
 def test_image_rgb(systematic, log, sparse):
 
-    n = 26
-    d_v = 1
-    d_c = 2
+    n = 69
+    d_v = 2
+    d_c = 3
     seed = 0
     rnd = np.random.RandomState(seed)
     H, G = make_ldpc(n, d_v, d_c, seed=seed, systematic=systematic,
@@ -63,9 +60,9 @@ def test_image_rgb(systematic, log, sparse):
 @pytest.mark.parametrize("systematic, log, sparse",
                          product([True], [True, False], [False, True]))
 def test_image_row(systematic, log, sparse):
-    n = 74
-    d_v = 1
-    d_c = 2
+    n = 213
+    d_v = 2
+    d_c = 3
     seed = 0
     rnd = np.random.RandomState(seed)
     H, G = make_ldpc(n, d_v, d_c, seed=seed, systematic=systematic,
@@ -82,26 +79,3 @@ def test_image_row(systematic, log, sparse):
     x = ldpc_images.decode_img_rowbyrow(G, H, coded, snr, maxiter=100, log=log)
 
     assert ldpc_images.ber_img(img_bin, rgb2bin(x)) == 0
-
-
-# @pytest.mark.parametrize("systematic, log, sparse",
-#                          product([False, True], [True], [False, True]))
-# def test_image_rgb(systematic, log, sparse):
-#     n = 15
-#     d_v = 4
-#     d_c = 5
-#     seed = 0
-#
-#     H, G = make_ldpc(n, d_v, d_c, seed=seed, systematic=systematic)
-#     assert not binaryproduct(H, G).any()
-#
-#     n, k = G.shape
-#     snr = 100
-#
-#     v = np.arange(k) % 2
-#     y = encode(G, v, snr, seed)
-#
-#     d = decode(H, y, snr, maxiter=10, log=log)
-#     x = get_message(G, d)
-#
-#     assert abs(v - x).sum() == 0
