e05beed8ff7f76214b181f734898aa2aff6f6831
Hicham Janati <hicham.janati@inria.fr>
2019-12-24 12:09:43 -0800

    improve docstrings

--- pyldpc/encoder.py
+++ pyldpc/encoder.py
@@ -3,36 +3,21 @@ from . import utils
 
 
 def encode_random_message(tG, snr, seed=None):
-    """
-    IMPORTANT: tG can be transposed coding matrix scipy.sparse.csr_matrix
-    object to speed up calculations. Randomly computes a k-bits message v,
-    where G's shape is (k,n). And sends it through the canal.
+    """Encodes a random message given a generating matrix tG and a SNR.
 
-    Message v is passed to G: d = v,G. d is a n-vector turned into a BPSK
-    modulated vector x. Then Additive White Gaussian Noise (AWGN) is added.
+    Parameters
+    ----------
 
-    SNR is the Signal-Noise Ratio: SNR = 10log(1/variance) in decibels, where
-    variance is the variance of the AWGN.
-    Remember:
-        1. d = v.G => tG.tv = td
-        2. x = BPSK(d) (or if you prefer the math: x = pow(-1,d) )
-        3. y = x + AWGN(0, snr)
+    tG: array or scipy.sparse.csr_matrix (m, k). Transposed coding matrix
+    obtained from `pyldpc.make_ldpc`.
 
-    ----------------------------
-
-    Parameters:
-
-    tG: 2D-Array (OR scipy.sparse.csr_matrix) Transpose of Coding Matrix
-    obtained from CodingMatrix functions.
-
-    snr: the Signal-Noise Ratio: SNR = 10log(1 / variance) in decibels.
-        >> snr = 10log(1 / variance)
-
-    -------------------------------
+    snr: float. Signal-Noise Ratio. SNR = 10log(1 / variance) in decibels.
 
     Returns
+    -------
 
-    Tuple(v,y) (Returns v to keep track of the random message v)
+    v: array (k,) random message generated.
+    y: array (n,) coded message + noise.
 
     """
     rnd = np.random.RandomState(seed)
@@ -41,7 +26,7 @@ def encode_random_message(tG, snr, seed=None):
     v = rnd.randint(2, size=k)
 
     d = utils.binaryproduct(tG, v)
-    x = pow(-1, d)
+    x = (-1) ** d
 
     sigma = 10 ** (-snr / 20)
 
@@ -53,31 +38,22 @@ def encode_random_message(tG, snr, seed=None):
 
 
 def encode(tG, v, snr, seed=None):
-    """
+    """Encodes a binary message and adds Gaussian noise.
 
-    IMPORTANT: if H is large, tG can be transposed coding matrix
-    scipy.sparse.csr_matrix object to speed up calculations.
-    Codes a message v with Coding Matrix G, and sends it through a
-    noisy (default) channel. Message v is passed to tG: d = tG.tv d is a
-    n-vector turned into a BPSK modulated vector x. Then Additive White
-    Gaussian Noise (AWGN) is added.
-    SNR is the Signal-Noise Ratio: SNR = 10log(1/variance) in decibels,
-    where variance is the variance of the AWGN.
+    Parameters
+    ----------
 
-        1. d = v.G (or (td = tG.tv))
-        2. x = BPSK(d) (or if you prefer the math: x = pow(-1,d) )
-        3. y = x + AWGN(0,snr)
+    tG: array or scipy.sparse.csr_matrix (m, k). Transposed coding matrix
+    obtained from `pyldpc.make_ldpc`.
 
-    Parameters:
+    v: array (k,) binary message to be encoded.
 
-    tG: 2D-Array (OR scipy.sparse.csr_matrix)Transposed Coding Matrix obtained
-    from coding_matrix functions. v: 1D-Array, k-vector (binary of course ..)
-    SNR: Signal-Noise-Ratio: SNR = 10log(1/variance) in decibels.
+    snr: float. Signal-Noise Ratio. SNR = 10log(1 / variance) in decibels.
 
-    -------------------------------
+    Returns
+    -------
 
-    Returns y
-
+    y: array (n,) coded message + noise.
     """
     n, k = tG.shape
 
