2665f67edfe017816a334e650bf6e63d816098df
Hicham Janati <hicham.janati@inria.fr>
2019-12-25 19:41:35 -0800

    add install requires

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ def readme():
         return f.read()
 
 
+INSTALL_REQUIRES = ['numpy', 'scipy', 'numba']
+
 EXTRAS_REQUIRE = {'tests': ['pytest', 'pytest-cov'],
                   'docs': ['sphinx', 'sphinx-gallery',
                            'sphinx_rtd_theme', 'numpydoc',
@@ -19,7 +21,8 @@ if __name__ == "__main__":
           include_dirs=[np.get_include()],
           extras_require=EXTRAS_REQUIRE,
           version='0.7.8',
-          description='Simulation of Low Density Parity Check Codes ldpc',
+          description='Simulation of Low Density Parity Check (LDPC) Codes',
+          install_requires=INSTALL_REQUIRES,
           long_description=readme(),
           classifiers=[
               'Programming Language :: Python :: 3.6',
@@ -30,7 +33,7 @@ if __name__ == "__main__":
               'Intended Audience :: Telecommunications Industry',
               'Natural Language :: English',
           ],
-          keywords='codes ldpc error detection decoding coding pyldpc',
+          keywords='codes ldpc error decoding information theory',
           url='https://github.com/hichamjanati/pyldpc',
           author='Hicham Janati',
           author_email='hicham.janati100@gmail.com',
